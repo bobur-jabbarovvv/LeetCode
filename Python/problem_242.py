48d59504f744415e70c019d40e84fed5ea9c6a2b
bobur-jabbarovvv <bobur.jabbarovvv@gmail.com>
2024-11-30 21:33:10 +0500

    Valid Anagram

---
+++ Python/problem_242.py
@@ -0,0 +1,22 @@
+"""
+242. Valid Anagram
+
+Given two strings s and t,
+return true if t is an anagramof s,
+and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+"""
+
+# Solution
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        return sorted(s) == sorted(t)
