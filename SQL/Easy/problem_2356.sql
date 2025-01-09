44eb8ec6a2f5f322e23c30bce5598d1c83e211df
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-09 18:36:17 +0500

    Number of Unique Subjects Taught by Each Teacher

--- SQL/Easy/problem_2356.sql
+++ SQL/Easy/problem_2356.sql
@@ -41,14 +41,14 @@ Output:
     | 2          | 4   |
     +------------+-----+
 Explanation: 
-Teacher 1:
-  - They teach subject 2 in departments 3 and 4.
-  - They teach subject 3 in department 3.
-Teacher 2:
-  - They teach subject 1 in department 1.
-  - They teach subject 2 in department 1.
-  - They teach subject 3 in department 1.
-  - They teach subject 4 in department 1.
+    Teacher 1:
+    - They teach subject 2 in departments 3 and 4.
+    - They teach subject 3 in department 3.
+    Teacher 2:
+    - They teach subject 1 in department 1.
+    - They teach subject 2 in department 1.
+    - They teach subject 3 in department 1.
+    - They teach subject 4 in department 1.
 */
 
 -- Solution
