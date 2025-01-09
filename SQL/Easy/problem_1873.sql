c83683040225ed9f4f0b2348d4920f21ba1ef74d
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-09 18:42:39 +0500

    Calculate Special Bonus

--- SQL/Easy/problem_1873.sql
+++ SQL/Easy/problem_1873.sql
@@ -17,7 +17,6 @@ The bonus of an employee is 100% of their salary
 if the ID of the employee is an odd number and
 the employee's name does not start with the character 'M'.
 The bonus of an employee is 0 otherwise.
-
 Return the result table ordered by employee_id.
 The result format is in the following example.
 
