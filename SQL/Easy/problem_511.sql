8b663c219c8942a74a9c6f53e4204f52537e179f
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-09 18:35:33 +0500

    Game Play Analysis I

--- SQL/Easy/problem_511.sql
+++ SQL/Easy/problem_511.sql
@@ -10,18 +10,17 @@ Table: Activity
     | event_date   | date    |
     | games_played | int     |
     +--------------+---------+
-(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+(player_id, event_date) is the primary key
+(combination of columns with unique values) of this table.
 This table shows the activity of players of some games.
 Each row is a record of a player who logged in and played a number of games (possibly 0)
 before logging out on someday using some device.
  
-
 Write a solution to find the first login date for each player.
 Return the result table in any order.
 The result format is in the following example.
 
 Example 1:
-
 Input: 
 Activity table:
     +-----------+-----------+------------+--------------+
@@ -45,6 +44,10 @@ Output:
 
 -- Solution
 
-SELECT player_id, MIN(event_date) AS first_login
-FROM Activity
-GROUP BY player_id
+SELECT
+    player_id,
+    MIN(event_date) AS first_login
+FROM
+    Activity
+GROUP BY
+    player_id
