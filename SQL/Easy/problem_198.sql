f73097e89c57b36d7497d6d93683bb1d0775d937
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-09 18:37:36 +0500

    Delete Duplicate Emails

--- SQL/Easy/problem_198.sql
+++ SQL/Easy/problem_198.sql
@@ -15,13 +15,11 @@ Each row of this table contains an email. The emails will not contain uppercase 
 Write a solution to delete all duplicate emails,
 keeping only one unique email with the smallest id.
 For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
-
 For Pandas users, please note that you are supposed to modify Person in place.
 
 After running your script, the answer shown is the Person table.
 The driver will first compile and run your piece of code and then show the Person table.
 The final order of the Person table does not matter.
-
 The result format is in the following example.
 
 Example 1:
@@ -41,13 +39,18 @@ Output:
     | 1  | john@example.com |
     | 2  | bob@example.com  |
     +----+------------------+
-Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+Explanation:
+    john@example.com is repeated two times. We keep the row with the smallest Id = 1.
 */
 
 -- Solution
 
-DELETE p2
-FROM Person p1
-JOIN Person p2
-ON p1.email = p2.email
-AND p1.id < p2.id;
+DELETE
+    p2
+FROM
+    Person p1
+JOIN
+    Person p2
+ON
+    p1.email = p2.email AND
+    p1.id < p2.id
