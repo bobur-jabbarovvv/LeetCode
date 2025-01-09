c9e4088eca4b29a8ec0ee4ed9472bf313286e5c4
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-09 11:24:54 +0500

    User Activity for the Past 30 Days I

--- SQL/Easy/problem_1141.sql
+++ SQL/Easy/problem_1141.sql
@@ -49,12 +49,18 @@ Output:
     | 2019-07-20 | 2            |
     | 2019-07-21 | 2            |
     +------------+--------------+ 
-Explanation: Note that we do not care about days with zero active users.
+Explanation:
+    Note that we do not care about days with zero active users.
 */
 
 -- Solution
 
-SELECT activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
-FROM Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-GROUP BY activity_date
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT(user_id)) AS active_users
+FROM
+    Activity
+WHERE
+    activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY
+    activity_date
