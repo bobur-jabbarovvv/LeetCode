016db4bba8aec03b4f438b6d97c0d2a4c3884817
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-10 11:36:34 +0500

    Height Checker

--- Python/Easy/problem_1051.py
+++ Python/Easy/problem_1051.py
@@ -1,7 +1,7 @@
 """
 1051. Height Checker
 
-    A school is trying to take an annual photo of all the students.
+A school is trying to take an annual photo of all the students.
 The students are asked to stand in a single file line in non-decreasing order by height.
 Let this ordering be represented by the integer array expected
 where expected[i] is the expected height of the ith student in line.
@@ -11,29 +11,28 @@ Each heights[i] is the height of the ith student in line (0-indexed).
 Return the number of indices where heights[i] != expected[i].
 
 Example 1:
-Input: heights = [1,1,4,2,1,3]
-Output: 3
+    Input: heights = [1,1,4,2,1,3]
+    Output: 3
 Explanation: 
-heights:  [1,1,4,2,1,3]
-expected: [1,1,1,2,3,4]
-Indices 2, 4, and 5 do not match.
+    heights:  [1,1,4,2,1,3]
+    expected: [1,1,1,2,3,4]
+    Indices 2, 4, and 5 do not match.
 
 Example 2:
-Input: heights = [5,1,2,3,4]
-Output: 5
+    Input: heights = [5,1,2,3,4]
+    Output: 5
 Explanation:
-heights:  [5,1,2,3,4]
-expected: [1,2,3,4,5]
-All indices do not match.
+    heights:  [5,1,2,3,4]
+    expected: [1,2,3,4,5]
+    All indices do not match.
 
 Example 3:
-Input: heights = [1,2,3,4,5]
-Output: 0
+    Input: heights = [1,2,3,4,5]
+    Output: 0
 Explanation:
-heights:  [1,2,3,4,5]
-expected: [1,2,3,4,5]
-All indices match.
-
+    heights:  [1,2,3,4,5]
+    expected: [1,2,3,4,5]
+    All indices match.
 """
 
 # Solution
