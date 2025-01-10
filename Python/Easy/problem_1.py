ef7f4a61bfb9cb05368c10366a969253ca6cf5c7
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-10 11:35:33 +0500

    Two Sum

--- Python/Easy/problem_1.py
+++ Python/Easy/problem_1.py
@@ -1,5 +1,4 @@
 """
-
 1. Two Sum
 
 Description
@@ -11,18 +10,18 @@ and you may not use the same element twice.
 You can return the answer in any order.
 
 Example 1:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+Explanation:
+    Because nums[0] + nums[1] == 9, we return [0, 1].
 
 Example 2:
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
 
 Example 3:
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
 """
 
 class Solution(object):
@@ -38,7 +37,3 @@ class Solution(object):
                     output.append(i)
                     output.append(j)
         return output
-        
-
-
-
