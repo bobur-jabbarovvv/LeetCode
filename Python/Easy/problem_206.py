4728f467db805e99c944f3278a7b7f67653468d4
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-10 23:32:34 +0500

    Reverse Linked List

---
+++ Python/Easy/problem_206.py
@@ -0,0 +1,45 @@
+"""
+206. Reverse Linked List
+
+Given the head of a singly linked list,
+reverse the list, and return the reversed list.
+
+Example 1:
+Input:
+    head = [1,2,3,4,5]
+Output:
+    [5,4,3,2,1]
+
+Example 2:
+Input:
+    head = [1,2]
+Output:
+    [2,1]
+
+Example 3:
+Input:
+    head = []
+Output:
+    []
+"""
+
+# Solution
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        previos = None
+        current = head
+        
+        while current != None:
+            nextNode = current.next
+            current.next = previos
+            previos = current
+            current = nextNode
+        
+        return previos 
