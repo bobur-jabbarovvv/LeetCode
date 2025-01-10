04d9b3ba6d88cd030e8600ab1d257216fda9ff67
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-10 10:56:56 +0500

    Queries Quality and Percentage

--- SQL/Easy/problem_1211.sql
+++ SQL/Easy/problem_1211.sql
@@ -14,22 +14,17 @@ This table may have duplicate rows.
 This table contains information collected from some queries on a database.
 The position column has a value from 1 to 500.
 The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
- 
-
 We define query quality as:
     The average of the ratio between query rating and its position.
-
 We also define poor query percentage as:
     The percentage of all queries with rating less than 3.
 
 Write a solution to find each query_name, the quality and poor_query_percentage.
 Both quality and poor_query_percentage should be rounded to 2 decimal places.
 Return the result table in any order.
-
 The result format is in the following example.
 
 Example 1:
-
 Input: 
 Queries table:
     +------------+-------------------+----------+--------+
@@ -49,11 +44,9 @@ Output:
     | Dog        | 2.50    | 33.33                 |
     | Cat        | 0.66    | 33.33                 |
     +------------+---------+-----------------------+
-
 Explanation: 
     Dog queries quality is ((5 / 1) + (5 / 2) + (1 / 200)) / 3 = 2.50
     Dog queries poor_ query_percentage is (1 / 3) * 100 = 33.33
-
     Cat queries quality equals ((2 / 5) + (3 / 3) + (4 / 7)) / 3 = 0.66
     Cat queries poor_ query_percentage is (1 / 3) * 100 = 33.33
 */
@@ -69,4 +62,4 @@ FROM
 GROUP BY 
     query_name
 HAVING
-    query_name IS NOT NULL;
+    query_name IS NOT NULL
