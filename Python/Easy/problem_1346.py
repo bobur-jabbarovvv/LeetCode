967d0b47a40c3434fc253c954239e738fd3b0907
Bobur Jabbarov <125908068+bobur-jabbarovvv@users.noreply.github.com>
2025-01-10 11:37:26 +0500

    Check If N and Its Double Exist

--- Python/Easy/problem_1346.py
+++ Python/Easy/problem_1346.py
@@ -8,14 +8,16 @@ check if there exist two indices i and j such that:
     arr[i] == 2 * arr[j]
  
 Example 1:
-Input: arr = [10,2,5,3]
-Output: true
-Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+    Input: arr = [10,2,5,3]
+    Output: true
+Explanation:
+    For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
 
 Example 2:
-Input: arr = [3,1,7,11]
-Output: false
-Explanation: There is no i and j that satisfy the conditions.
+    Input: arr = [3,1,7,11]
+    Output: false
+Explanation:
+    There is no i and j that satisfy the conditions.
 """
 
 # Solution
