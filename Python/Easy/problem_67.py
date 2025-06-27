36340854b1db0e058f4cfc6c052111234e1afdbb
bobur-jabbarovvv <bobur.jabbarovvv@gmail.com>
2025-06-27 11:33:45 +0500

    67. Add Binary

--- Python/Easy/problem_67.py
+++ Python/Easy/problem_67.py
@@ -1,7 +1,8 @@
 """
 67. Add Binary
 
-Given two binary strings a and b, return their sum as a binary string.
+Given two binary strings a and b,
+return their sum as a binary string.
 
 Example 1:
     Input: a = "11", b = "1"
@@ -11,8 +12,6 @@ Example 2:
     Input: a = "1010", b = "1011"
     Output: "10101"
 """
-
-# Solution
 
 class Solution(object):
     def pow(self, x, n):
